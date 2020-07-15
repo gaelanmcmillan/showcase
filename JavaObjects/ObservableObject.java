c5e1b7e07fafea739fc365cff945ed56fffd759a
Andrew Meijer <andrew.tr.meijer@gmail.com>
2020-07-14 23:23:27 -0700

    Uploading Progress from old machine

---
+++ JavaObjects/ObservableObject.java
@@ -0,0 +1,20 @@
+/*
+ *  NAME:   ObservableObject.java
+ *  AUTHOR: Andrew Meijer
+ *  DATE:   June 12, 2020
+ *  PURPOSE: ObservableObject is a superclass for objects
+ *           that exist in test space.
+ */
+
+package JavaObjects;
+
+public class ObservableObject {
+    //variables
+
+    //constructors
+    public ObservableObject(){
+
+    }
+
+    //methods
+}
