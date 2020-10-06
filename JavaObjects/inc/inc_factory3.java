54def41fb8ea188c1b6b4447328662555991ad8f
Andrew Meijer <andrew.tr.meijer@gmail.com>
2020-10-06 02:07:21 -0700

    JavaObjects inc open-source factory samples downloaded

---
+++ JavaObjects/inc/inc_factory3.java
@@ -0,0 +1,8 @@
+/*
+ * NAME:    inc_factory3.java
+ * AUTHOR:  Journal Dev
+ *          https://www.journaldev.com/1392/factory-design-pattern-in-java
+ * DATE:    Transposed by Andrew Meijer, Oct. 6, 2020     
+ * PURPOSE: 
+ */
+
